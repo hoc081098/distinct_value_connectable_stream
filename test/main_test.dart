18a7ce39fc020c829e49dd2a78c854c9ec495b97
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-02-01 01:30:10 +0700

    :white_check_mark: Adding tests.

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,6 +1,8 @@
 import 'distinct_value_connectable_stream_test.dart'
     as distinct_value_connectable_stream_test;
+import 'distinct_value_test.dart' as distinct_value_test;
 
 void main() {
   distinct_value_connectable_stream_test.main();
+  distinct_value_test.main();
 }
