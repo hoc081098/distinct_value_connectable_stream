9b3e0086583c9cc6a571913569229cd46b178e58
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-03-21 00:22:32 +0700

    update tests

--- test/single_subscription_to_broadcast_test.dart
+++ test/single_subscription_to_broadcast_test.dart
@@ -5,11 +5,7 @@ void main() {
   group(
     'Single-subscription DistinctValueStream to broadcast DistinctValueStream',
     () {
-      test('asBroadcastDistinctValueStream', () async {
-        final stream = Stream.fromIterable([0, 1, 1, 2, 3, 4, 4])
-            .distinctValue(0)
-            .asBroadcastDistinctValueStream();
-
+      Future<void> _test(DistinctValueStream<int> stream) async {
         expect(stream.value, 0);
         expect(stream.isBroadcast, true);
 
@@ -18,6 +14,23 @@ void main() {
             stream, emitsInOrder(<Object>[1, 2, 3, 4, emitsDone]));
 
         expect(stream, emitsDone);
+      }
+
+      test('SingleSubscription.asBroadcastDistinctValueStream', () async {
+        final stream = Stream.fromIterable([0, 1, 1, 2, 3, 4, 4])
+            .distinctValue(0)
+            .asBroadcastDistinctValueStream();
+
+        await _test(stream);
+      });
+
+      test('Broadcast.asBroadcastDistinctValueStream', () async {
+        final source =
+            Stream.fromIterable([0, 1, 1, 2, 3, 4, 4]).shareValueDistinct(0);
+        final stream = source.asBroadcastDistinctValueStream();
+
+        expect(identical(stream, source), true);
+        await _test(stream);
       });
     },
   );
