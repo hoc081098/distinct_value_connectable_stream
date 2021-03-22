559ebab4bafb3c804afdbe9c6e910a03e5c673e6
Nguyễn Thái Học <hoc081098@gmail.com>
2021-03-22 08:58:32 +0700

    more tests

--- lib/src/distinct_value_connectable_stream.dart
+++ lib/src/distinct_value_connectable_stream.dart
@@ -95,6 +95,7 @@ class _DistinctValueConnectableStream<T>
   DistinctValueStream<T> autoConnect({
     void Function(StreamSubscription<T> subscription)? connection,
   }) {
+    print('auto');
     _checkUsed();
 
     _subject.onListen = () {
@@ -108,6 +109,7 @@ class _DistinctValueConnectableStream<T>
 
   @override
   StreamSubscription<T> connect() {
+    print('conn');
     _checkUsed();
 
     _subject.onListen = _subject.onCancel = null;
@@ -116,6 +118,7 @@ class _DistinctValueConnectableStream<T>
 
   @override
   DistinctValueStream<T> refCount() {
+    print('ref');
     _checkUsed();
 
     late ConnectableStreamSubscription<T> subscription;
--- test/single_subscription_to_broadcast_test.dart
+++ test/single_subscription_to_broadcast_test.dart
@@ -2,36 +2,66 @@ import 'package:distinct_value_connectable_stream/distinct_value_connectable_str
 import 'package:test/test.dart';
 
 void main() {
-  group(
-    'Single-subscription DistinctValueStream to broadcast DistinctValueStream',
-    () {
+  final elements = [0, 1, 1, 2, 3, 4, 4];
+  final expected = <Object>[1, 2, 3, 4, emitsDone];
+
+  group('To broadcast DistinctValueStream', () {
+    group('asBroadcastDistinctValueStream', () {
       Future<void> _test(DistinctValueStream<int> stream) async {
         expect(stream.value, 0);
         expect(stream.isBroadcast, true);
 
-        expect(stream, emitsInOrder(<Object>[1, 2, 3, 4, emitsDone]));
-        await expectLater(
-            stream, emitsInOrder(<Object>[1, 2, 3, 4, emitsDone]));
+        expect(stream, emitsInOrder(expected));
+        await expectLater(stream, emitsInOrder(expected));
 
         expect(stream, emitsDone);
       }
 
-      test('SingleSubscription.asBroadcastDistinctValueStream', () async {
-        final stream = Stream.fromIterable([0, 1, 1, 2, 3, 4, 4])
+      test('Single-Subscription', () async {
+        final stream = Stream.fromIterable(elements)
             .distinctValue(0)
             .asBroadcastDistinctValueStream();
 
         await _test(stream);
       });
 
-      test('Broadcast.asBroadcastDistinctValueStream', () async {
-        final source =
-            Stream.fromIterable([0, 1, 1, 2, 3, 4, 4]).shareValueDistinct(0);
+      test('Broadcast', () async {
+        final source = Stream.fromIterable(elements).shareValueDistinct(0);
         final stream = source.asBroadcastDistinctValueStream();
 
         expect(identical(stream, source), true);
         await _test(stream);
       });
-    },
-  );
+    });
+
+    group('asDistinctValueConnectableStream', () {
+      Future<void> _test(DistinctValueConnectableStream<int> stream) async {
+        expect(stream.value, 0);
+        expect(stream.isBroadcast, true);
+
+        stream.connect();
+
+        expect(stream, emitsInOrder(expected));
+        await expectLater(stream, emitsInOrder(expected));
+
+        expect(stream, emitsDone);
+      }
+
+      test('Single-Subscription', () async {
+        final stream = Stream.fromIterable(elements)
+            .distinctValue(0)
+            .asDistinctValueConnectableStream();
+
+        await _test(stream);
+      });
+
+      test('Broadcast', () async {
+        final source = Stream.fromIterable(elements).publishValueDistinct(0);
+        final stream = source.asDistinctValueConnectableStream();
+
+        expect(identical(stream, source), true);
+        await _test(stream);
+      });
+    });
+  });
 }
