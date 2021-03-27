e25b034e5cd03cab8ea5949184dc9c50c168a380
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2021-03-27 17:15:43 +0700

    docs

--- lib/src/distinct_value_subject.dart
+++ lib/src/distinct_value_subject.dart
@@ -6,7 +6,36 @@ import 'package:rxdart_ext/rxdart_ext.dart'
 
 import 'distinct_value_stream.dart';
 
-/// TODO
+/// A special [StreamController] that captures the latest item that has been
+/// added to the controller.
+///
+/// [DistinctValueSubject] is the same as [PublishSubject] and [ValueSubject], with the ability to capture
+/// the latest item has been added to the controller.
+/// [DistinctValueSubject] is a [DistinctValueStream], that provides synchronous access to the last emitted item,
+/// and two consecutive values are not equal.
+/// The equality between previous data event and current data event is determined by [equals].
+///
+/// [DistinctValueSubject] is, by default, a broadcast (aka hot) controller, in order
+/// to fulfill the Rx Subject contract. This means the Subject's `stream` can
+/// be listened to multiple times.
+///
+/// ### Example
+///
+///     final subject = DistinctValueSubject<int>(1);
+///
+///     print(subject.value);          // prints 1
+///
+///     // observers will receive 2, 3 and done events.
+///     subject.stream.listen(print); // prints 2, 3
+///     subject.stream.listen(print); // prints 2, 3
+///     subject.stream.listen(print); // prints 2, 3
+///
+///     subject.add(1);
+///     subject.add(1);
+///     subject.add(2);
+///     subject.add(2);
+///     subject.add(3);
+///     subject.close();
 @sealed
 class DistinctValueSubject<T> extends Subject<T>
     implements DistinctValueStream<T> {
@@ -20,7 +49,13 @@ class DistinctValueSubject<T> extends Subject<T>
     this._subject,
   ) : super(_subject, _subject.stream);
 
-  /// TODO
+  /// Constructs a [DistinctValueSubject], optionally pass handlers for
+  /// [onListen], [onCancel] and a flag to handle events [sync].
+  ///
+  /// [seedValue] becomes the current [value] of Subject.
+  /// [equals] is used to determine equality between previous data event and current data event.
+  ///
+  /// See also [StreamController.broadcast],  [ValueSubject].
   factory DistinctValueSubject(
     T seedValue, {
     bool Function(T p1, T p2)? equals,
